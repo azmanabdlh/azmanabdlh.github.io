44993e25455bc3c9c3c0d800865a2f8479ac57bc
azmanabdlh <azman.abdullah@tokopedia.com>
2024-01-27 22:41:30 +0900

    change flower

---
+++ manifest_FQQgjhDj.mjs
@@ -0,0 +1,80 @@
+import '@astrojs/internal-helpers/path';
+import 'cookie';
+import 'kleur/colors';
+import 'string-width';
+import 'html-escaper';
+import 'clsx';
+import './public/chunk.REQqzKW8.mjs';
+import { compile } from 'path-to-regexp';
+
+if (typeof process !== "undefined") {
+  let proc = process;
+  if ("argv" in proc && Array.isArray(proc.argv)) {
+    if (proc.argv.includes("--verbose")) ; else if (proc.argv.includes("--silent")) ; else ;
+  }
+}
+
+function getRouteGenerator(segments, addTrailingSlash) {
+  const template = segments.map((segment) => {
+    return "/" + segment.map((part) => {
+      if (part.spread) {
+        return `:${part.content.slice(3)}(.*)?`;
+      } else if (part.dynamic) {
+        return `:${part.content}`;
+      } else {
+        return part.content.normalize().replace(/\?/g, "%3F").replace(/#/g, "%23").replace(/%5B/g, "[").replace(/%5D/g, "]").replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      }
+    }).join("");
+  }).join("");
+  let trailing = "";
+  if (addTrailingSlash === "always" && segments.length) {
+    trailing = "/";
+  }
+  const toPath = compile(template + trailing);
+  return toPath;
+}
+
+function deserializeRouteData(rawRouteData) {
+  return {
+    route: rawRouteData.route,
+    type: rawRouteData.type,
+    pattern: new RegExp(rawRouteData.pattern),
+    params: rawRouteData.params,
+    component: rawRouteData.component,
+    generate: getRouteGenerator(rawRouteData.segments, rawRouteData._meta.trailingSlash),
+    pathname: rawRouteData.pathname || void 0,
+    segments: rawRouteData.segments,
+    prerender: rawRouteData.prerender,
+    redirect: rawRouteData.redirect,
+    redirectRoute: rawRouteData.redirectRoute ? deserializeRouteData(rawRouteData.redirectRoute) : void 0,
+    fallbackRoutes: rawRouteData.fallbackRoutes.map((fallback) => {
+      return deserializeRouteData(fallback);
+    })
+  };
+}
+
+function deserializeManifest(serializedManifest) {
+  const routes = [];
+  for (const serializedRoute of serializedManifest.routes) {
+    routes.push({
+      ...serializedRoute,
+      routeData: deserializeRouteData(serializedRoute.routeData)
+    });
+    const route = serializedRoute;
+    route.routeData = deserializeRouteData(serializedRoute.routeData);
+  }
+  const assets = new Set(serializedManifest.assets);
+  const componentMetadata = new Map(serializedManifest.componentMetadata);
+  const clientDirectives = new Map(serializedManifest.clientDirectives);
+  return {
+    ...serializedManifest,
+    assets,
+    componentMetadata,
+    clientDirectives,
+    routes
+  };
+}
+
+const manifest = deserializeManifest({"adapterName":"","routes":[{"file":"","links":[],"scripts":[{"type":"inline","value":"const s=window.AudioContext||window.webkitAudioContext,e=new s,n=document.getElementById(\"btn-play-pause\"),t=document.getElementById(\"my-player\"),a=e.createMediaElementSource(t);a.connect(e.destination);const i='<svg class=\"h-7 w-7\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"currentColor\"><path d=\"M19.376 12.4161L8.77735 19.4818C8.54759 19.635 8.23715 19.5729 8.08397 19.3432C8.02922 19.261 8 19.1645 8 19.0658V4.93433C8 4.65818 8.22386 4.43433 8.5 4.43433C8.59871 4.43433 8.69522 4.46355 8.77735 4.5183L19.376 11.584C19.6057 11.7372 19.6678 12.0477 19.5146 12.2774C19.478 12.3323 19.4309 12.3795 19.376 12.4161Z\"></path></svg>',l='<svg class=\"h-7 w-7\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"currentColor\"><path d=\"M6 5H8V19H6V5ZM16 5H18V19H16V5Z\"></path></svg>';let o=!1;e.state===\"suspended\"&&e.resume();t.play();n.addEventListener(\"click\",()=>{if(e.state===\"suspended\"&&e.resume(),o){o=!1,t.play(),n.innerHTML=l;return}o=!0,t.pause(),n.innerHTML=i});\n"}],"styles":[{"type":"external","src":"/public/asset.jYFPABxr.css"}],"routeData":{"route":"/wedding/[id]","type":"page","pattern":"^\\/wedding\\/([^/]+?)\\/?$","segments":[[{"content":"wedding","dynamic":false,"spread":false}],[{"content":"id","dynamic":true,"spread":false}]],"params":["id"],"component":"src/pages/wedding/[id].astro","prerender":false,"fallbackRoutes":[],"_meta":{"trailingSlash":"ignore"}}}],"site":"https://azmanabdlh.github.io","base":"/","trailingSlash":"ignore","compressHTML":true,"componentMetadata":[["/Users/azman.abdullah/Repositories/astro/azman-wedding/src/pages/wedding/[id].astro",{"propagation":"none","containsHead":true}]],"renderers":[],"clientDirectives":[["idle","(()=>{var i=t=>{let e=async()=>{await(await t())()};\"requestIdleCallback\"in window?window.requestIdleCallback(e):setTimeout(e,200)};(self.Astro||(self.Astro={})).idle=i;window.dispatchEvent(new Event(\"astro:idle\"));})();"],["load","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).load=e;window.dispatchEvent(new Event(\"astro:load\"));})();"],["media","(()=>{var s=(i,t)=>{let a=async()=>{await(await i())()};if(t.value){let e=matchMedia(t.value);e.matches?a():e.addEventListener(\"change\",a,{once:!0})}};(self.Astro||(self.Astro={})).media=s;window.dispatchEvent(new Event(\"astro:media\"));})();"],["only","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).only=e;window.dispatchEvent(new Event(\"astro:only\"));})();"],["visible","(()=>{var r=(i,c,s)=>{let n=async()=>{await(await i())()},t=new IntersectionObserver(e=>{for(let o of e)if(o.isIntersecting){t.disconnect(),n();break}});for(let e of s.children)t.observe(e)};(self.Astro||(self.Astro={})).visible=r;window.dispatchEvent(new Event(\"astro:visible\"));})();"]],"entryModules":{"\u0000astro-internal:middleware":"_astro-internal_middleware.mjs","\u0000@astro-page:src/pages/wedding/[id]@_@astro":"pages/wedding/_id_.astro.mjs","\u0000@astro-renderers":"renderers.mjs","/src/pages/wedding/[id].astro":"public/chunk.epX9AzQ2.mjs","\u0000@astrojs-manifest":"manifest_FQQgjhDj.mjs","/Users/azman.abdullah/Repositories/astro/azman-wedding/node_modules/@astrojs/react/vnode-children.js":"public/chunk.3wEZly-Z.mjs","@templates/JumbotronInvitation":"public/entry.a87dU-sB.mjs","@templates/About":"public/entry.ke_X2kuy.mjs","/astro/hoisted.js?q=0":"public/entry.IWT7sPE9.mjs","@templates/Gift":"public/entry.2V4gtN0U.mjs","@astrojs/react/client.js":"public/entry.RKUNzFsQ.mjs","astro:scripts/before-hydration.js":""},"assets":[]});
+
+export { manifest };
